0cebc26e4c98a2845146ebc8611c0006d2675cd8
valentinefleith <valentine.fleith@gmail.com>
2024-03-09 11:45:26 +0100

    add lemmas

--- src/preprocessing/clean_corpus.py
+++ src/preprocessing/clean_corpus.py
@@ -1,5 +1,6 @@
 import glob
 import re
+import spacy
 
 CATEGORIES = ["adventure", "comedy", "family", "fantasy", "sci-fi"]
 
@@ -10,13 +11,19 @@ def tokenize(text: str) -> list[str]:
         for token in re.findall(r"\b\w+?\b(?:'|’)?", text)
     ]
 
+def lemmatize(text: str) -> list[str]:
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    lemmas = [token.lemma_ for token in doc]
+    return lemmas
+
 
 def main():
     all_files = glob.glob("corpus/*/*.txt")
     for path in all_files:
         with open(path, "r") as file:
             content = file.read()
-        tokens = tokenize(content)
+        tokens = lemmatize(content)
         with open(path, "w") as file:
             for token in tokens:
                 if token not in CATEGORIES:
