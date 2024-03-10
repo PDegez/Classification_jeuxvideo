f7720671642cc8596823188b9c486e6020919c23
valentinefleith <valentine.fleith@gmail.com>
2024-03-10 15:29:56 +0100

    leave possibility to split train test or cross validation

--- src/algorithms/knn.py
+++ src/algorithms/knn.py
@@ -1,6 +1,6 @@
 import sys
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_predict, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
@@ -23,17 +23,25 @@ def get_matrix(corpus):
     return matrix
 
 
-def knn_model(matrix, classes):
-    #x_train, x_test, y_train, y_test = train_test_split(matrix, classes, test_size=0.2, random_state=42)
+def knn_model_cross_val(matrix, classes):
     knn_classifier = KNeighborsClassifier(n_neighbors=9)
     y_pred = cross_val_predict(knn_classifier, matrix, classes, cv=5)
-    #scores = cross_val_score(knn_classifier, matrix, classes, cv=5)
-    #mean_accuracy = np.mean(scores)
-    #knn_classifier.fit(x_train, y_train)
-    #y_pred = knn_classifier.predict(x_test)
     accuracy = accuracy_score(classes, y_pred)
     conf_matrix = confusion_matrix(classes, y_pred)
     disp = ConfusionMatrixDisplay(conf_matrix, display_labels=np.unique(classes))
+    disp.plot()
+    plt.show()
+    return accuracy
+
+
+def knn_model_split_train(matrix, classes):
+    x_train, x_test, y_train, y_test = train_test_split(matrix, classes, test_size=0.2, random_state=42)
+    knn_classifier = KNeighborsClassifier(n_neighbors=9)
+    knn_classifier.fit(x_train, y_train)
+    y_pred = knn_classifier.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(conf_matrix, display_labels=knn_classifier.classes_)
     disp.plot()
     plt.show()
     return accuracy
@@ -45,7 +53,8 @@ def main():
     corpus = glob.glob(f"{sys.argv[1]}/*/*.txt")
     matrix = get_matrix(corpus)
     classes = [path.split("/")[-2] for path in corpus]
-    accuracy = knn_model(matrix, classes)
+    accuracy = knn_model_cross_val(matrix, classes)
+    #accuracy = knn_model_split_train(matrix, classes)
     print("Accuracy:", accuracy)
 
 
