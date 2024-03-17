7e895f183e60ea232db81447f3fb23216c223061
PDegez <pauline.degez@gmail.com>
2024-03-17 12:02:36 +0100

    script statistiques propres

--- statistiques/stats_descriptives.py
+++ statistiques/stats_descriptives.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 from pathlib import Path
-import sys
+import argparse
 import numpy as np
 import csv
 
 
+def parser():
+    parser = argparse.ArgumentParser(description="Statistiques descriptives du corpus")
+    parser.add_argument("-p", "--boxplots", action="store_true", help="Affichage des boxplots")
+    parser.add_argument("-c", "--csv", type=str, help="Chemin de l'output csv")
+    parser.add_argument("input_directory", help="Chemin vers le dossier contenant le corpus") 
+    args = parser.parse_args()
+    
+    return args
+
+
 def extraction_wc(dossier:Path)->list:
+    """extraction des longueurs des textes d'un dossier"""
+    
     liste_wc = []
     liste_path_textes = [f for f in dossier.iterdir() if f.is_file()]
     for texte in liste_path_textes:
@@ -14,56 +26,92 @@ def extraction_wc(dossier:Path)->list:
             liste_wc.append(fichier_wc)
     return liste_wc
 
+
 def plot_descriptifs(data: dict):
+    """affichage des statistiques descriptives des datas d'un dictionnaire dans
+    des boxplots"""
+    
+    # Création d'une liste géante pour pouvoir plus tard exclure les données
+    # abérantes
     all_data = np.concatenate(list(data.values()))
     fig, ax = plt.subplots()
+    
+    # Création des plots et coloration des boites
     boxplot = ax.boxplot(data.values(), patch_artist=True, showmeans=True)
+    
+    # Ajustement de la fenêtre pour exclure du visuel les 5% de données supérieures
+    # (qui sont abérantes)
     limite_superieure = np.percentile(all_data, 95)
-    ax.set_ylim(ax.get_ylim()[0], limite_superieure)
+    ax.set_ylim( 0, limite_superieure)
+    
+    # Changement des couleurs
     for box in boxplot['boxes']:
         box.set(facecolor='cyan')
     for median in boxplot['medians']:
         median.set(color='black')
     for mean in boxplot['means']:
         mean.set(color='white') 
+    
+    # Assignation des clés du dictionnaire aux labels des plots
     ax.set_xticklabels(data.keys())
 
     plt.show()
     
-
-
-chemin_dossier = Path(sys.argv[1])
-chemin_output = sys.argv[2]
-
-dossiers_categories = [d for d in chemin_dossier.iterdir() if d.is_dir()]
-#liste_fichiers = list(chemin_dossier.glob('**/*'))
-
-print("STATISTIQUES DESCRIPTIVES POUR NOS CATEGORIES : \n\n")
-data_plot = {}
-for categorie in chemin_dossier.iterdir():
-    categorie_wc_list = extraction_wc(categorie)
-    data_plot[categorie.name] = categorie_wc_list
-    print(f"{categorie.name.upper()}\nlongueur moyenne : {np.mean(categorie_wc_list)}\nlongueur max : {max(categorie_wc_list)}\nlongueur min : {min(categorie_wc_list)}\nécart type : {np.std(categorie_wc_list)}\nlongueur médiane : {np.percentile(categorie_wc_list, 50)}\npremier quartile : {np.percentile(categorie_wc_list, 25)}\ntroisième quartile : {np.percentile(categorie_wc_list, 75)}\n")
-
-
-with open(chemin_output, mode='a', newline='') as fichier_csv:
-    writer = csv.writer(fichier_csv)
     
-    writer.writerow(["", "LONGUEUR MOYENNE", "LONGUEUR MAX", "LONGUEUR MIN", "ECART TYPE", "LONGUEUR MEDIANE", "PREMIER QUARTILE", "TROISIEME QUARTILE"])
+def write_csv(data:dict, chemin:Path):
+    with open(chemin, mode='a', newline='') as fichier_csv:
+        writer = csv.writer(fichier_csv)
     
-    for categorie in data_plot.keys():
-        writer.writerow([
-            categorie.upper(),
-            np.mean(data_plot[categorie]),
-            max(data_plot[categorie]),
-            min(data_plot[categorie]),
-            np.std(data_plot[categorie]),
-            np.mean(data_plot[categorie]),
-            np.percentile((data_plot[categorie]), 25),
-            np.percentile((data_plot[categorie]), 75),
-            ])
+        writer.writerow(["",
+                         "LONGUEUR MOYENNE",
+                         "LONGUEUR MAX",
+                         "LONGUEUR MIN",
+                         "ECART TYPE",
+                         "LONGUEUR MEDIANE",
+                         "PREMIER QUARTILE",
+                         "TROISIEME QUARTILE"])
+    
+        for categorie in data.keys():
+            writer.writerow([
+                categorie.upper(),
+                np.mean(data[categorie]),
+                max(data[categorie]),
+                min(data[categorie]),
+                np.std(data[categorie]),
+                np.mean(data[categorie]),
+                np.percentile((data[categorie]), 25),
+                np.percentile((data[categorie]), 75),
+                ])
+            
+    return print(f"Le fichier csv a bien été créé au chemin {chemin}")    
 
-plot_descriptifs(data_plot)
+
+
+def main(dossier=None, plot=None, csv=None):
+    dossiers_categories = [d for d in dossier.iterdir() if d.is_dir()]
+
+    #Statistiques vers sortie standard
+    print("\nSTATISTIQUES DESCRIPTIVES POUR NOS CATEGORIES : \n")
+    data_plot = {}
+    for categorie in dossiers_categories:
+        if categorie.name != "Adventure":
+            categorie_wc_list = extraction_wc(categorie)
+            data_plot[categorie.name] = categorie_wc_list
+            print(f"{categorie.name.upper()}\nlongueur moyenne : {np.mean(categorie_wc_list)}\nlongueur max : {max(categorie_wc_list)}\nlongueur min : {min(categorie_wc_list)}\nécart type : {np.std(categorie_wc_list)}\nlongueur médiane : {np.percentile(categorie_wc_list, 50)}\npremier quartile : {np.percentile(categorie_wc_list, 25)}\ntroisième quartile : {np.percentile(categorie_wc_list, 75)}\n")
+
+    #Statistiques vers sortie csv
+    if csv:
+        write_csv(data_plot, Path(csv))
+    
+    #Statistiques : affichage des plots
+    if plot:
+        plot_descriptifs(data_plot)
+
+
+if __name__ == "__main__":
+    args=parser()
+    print(args)
+    main(dossier=Path(args.input_directory), plot=args.boxplots, csv=args.csv)
 
 
         
